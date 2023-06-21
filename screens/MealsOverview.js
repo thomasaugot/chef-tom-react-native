559d584ee0f0c5b18723c303fb946b57496f1544
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-21 20:20:03 +0200

    items page style and ok

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,11 +1,35 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const MealsOverview = ({ route }) => {
   const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        imageUrl={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => {
+          item.id;
+        }}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
